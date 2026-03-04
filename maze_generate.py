5cb950f15ccf075a4c4aaa6563bda0f914bfc7fc
Imad Laaouina <ilaaouin@c4r10p6.1337.ma>
2026-03-04 00:59:19 +0000

    adding cell and maze classes

---
+++ maze_generate.py
@@ -0,0 +1,39 @@
+class Cell:
+    def __init__(self, row, col):
+        self.row = row
+        self.col = col
+        self.walls = {
+            "top" : True,
+            "right" : True,
+            "down" : True,
+            "left" : True
+        }
+        self.visited = False
+
+
+class MazeGenerate:
+    def __init__(self, width, height):
+        self.width = width
+        self.height = height
+        self.maze = self.mazeGen()
+
+    def mazeGen(self):
+        mazz = []
+        for row in range(self.height):
+            lis = []
+            for coll in range(self.width):
+                cel = Cell(row, coll)
+                lis.append(cel)
+            mazz.append(lis)
+        return mazz
+
+def PrintMaze(maz):
+    for row in maz.maze:
+        for cel in row:
+            print("+", end=" ")
+        print()
+
+mazee = MazeGenerate(13, 13)
+PrintMaze(mazee)
+
+
